9d3b15557b77f46184b89d7b51d7214a4e6f2e58
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-01-02 19:46:39 +0530

    Minor change

--- schema/user.js
+++ schema/user.js
@@ -29,6 +29,7 @@ module.exports = {
                 age: {type: 'number'},
                 mobile: {
                     type: 'string',
+                    pattern: '[6-9][0-9]{9}',
                     minLength: 10,
                     maxLength: 10
                 }
