4598d1af13cca4181c2964da5860be2c4090a38e
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-02-07 16:46:26 +0530

    Add expiration date to user login and success bool

--- controller/user.js
+++ controller/user.js
@@ -22,17 +22,21 @@ exports.login = async (req, res) => {
 
                 return {
                     "code": 2,
+                    "success": true,
                     "message": "Login successful",
                     "token": token,
+                    "expiration-date": Date.now() + (24*6*60*60*1000)
                 }
             }
             return {
                 "code": 4,
+                "sucess": false,
                 "message": "Invalid password"
             }
         } else {
             return {
                 "code": 5,
+                "sucess": false,
                 "message": "No such user exists"
             }
         }
@@ -49,6 +53,7 @@ exports.signup = async (req, res) => {
     if (user) {
         return {
             "code": 3,
+            "sucess": false,
             "message": "Mobile already registered"
         }
     }
@@ -63,17 +68,18 @@ exports.signup = async (req, res) => {
             mobile
         });
 
-        const token = jwt.sign({
-            id: createdUser._id,
-            isUser: true
-        }, config.secret, {
-            expiresIn: 60 * 60 * 24 * 7
-        });
+        // const token = jwt.sign({
+        //     id: createdUser._id,
+        //     isUser: true
+        // }, config.secret, {
+        //     expiresIn: 60 * 60 * 24 * 7
+        // });
 
         return {
             "message": "Account created",
             "code": 1,
-            "token": token
+            "sucess": true,
+            // "token": token
         }
     } catch (err) {
         console.log(err);
